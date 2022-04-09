4dd64000ced4991aa5ad71c52b3ddaf22eec151a
cassidytrbl <97814148+cassidytrbl@users.noreply.github.com>
2022-04-09 12:46:41 -0700

    clear button

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,48 +5,11 @@ $("#currentDay").text(today.format("llll"));
 var searchButton = document.getElementById("searchTxt");
 searchButton.addEventListener("click", function (event) {});
 
-//var searchTxt = queryString.split("=")[1];
-var savedHike1 = document.querySelector("#savedHike1");
-var savedHike2 = document.querySelector("#savedHike2");
-//var savedHike3 = document.querySelector("#savedHike3");
-//var savedHike4 = document.querySelector("#savedHike4");
-
-savedHike1.addEventListener("click", function (event) {
-  localStorage.getItem();
-  console.log(event);
+$("#clearFieldsBtn").click(function (event) {
+  event.preventDefault;
+  $("savedHikes").val("");
+  localStorage.clear();
 });
-
-savedHike2.addEventListener("click", function (event) {
-  localStorage.getItem();
-});
-
-// savedHike3.addEventListener("click", function (event) {
-//localStorage.getItem();
-//});
-
-//savedHike4.addEventListener("click", function (event) {
-// localStorage.getItem();
-//});
-
-// savedHike4.addEventListener("click", function () {
-//   localStorage.getItem();
-// });
-
-// $("#clearFieldsBtn").click(function (event) {
-//   event.preventDefault;
-//   $("savedHikes").val("");
-//   localStorage.clear();
-// });
-
-// $("savedHikes").on("click", function () {
-//   typing = $(this).siblings(".typing").val();
-//   console.log(typing);
-//   localStorage.setItem(typing);
-// });
-
-// $("searchBar").val(localStorage.getItem("searchBar"));
-
-// Autocomplete search form widget
 
 $(function () {
   //commented out parks that did not return any results.
